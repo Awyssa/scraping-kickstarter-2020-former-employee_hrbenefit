4558e1833a2183fc3ecb3936271b2a1ba0ab91dc
Michael Ernest <michael.ehson@gmail.com>
2021-03-04 10:06:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,27 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'open-uri'
+
+require_relative './course.rb'
 
 def create_project_hash
-  # write your code here
 end
+
+
+
+
+
+
+# :projects => {
+#   "My Great Project"  => {
+#     :image_link => "Image Link",
+#     :description => "Description",
+#     :location => "Location",
+#     :percent_funded => "Percent Funded"
+#   },
+#   "Another Great Project" => {
+#     :image_link => "Image Link",
+#     :description => "Description",
+#     :location => "Location",
+#     :percent_funded => "Percent Funded"
+#   }
+# }
